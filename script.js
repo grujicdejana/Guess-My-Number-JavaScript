1c9f71f134ac3dc29b7d1a867eea8c50e3112937
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2021-01-23 17:13:54 +0100

    addEventListener for check button

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🎉 Correct Number! YOU WIN';
+    const score = document.querySelector('.score').textContent;
+    document.querySelector('.highscore').textContent = score;
+    //document.querySelector('.message').textContent = '🏆 You win!';
+  } else if (score > 0) {
+    if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '😬 Too high.';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😬 Too low.';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = '😒 GAME OVER! ';
+  }
+});
